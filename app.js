ab6469356bb4a8cb7f442e189568ce8363c239f6
Preet Singh <preet10syd@gmail.com>
2026-03-15 15:25:56 +1100

    thats it prject complete

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const path = require("node:path");
 const app = express();
 const PORT = 8080;
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended:true}))
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {messages: messages});
 });
 
 app.get("/new", (req, res) => {
-  res.render("newMessage");
+  res.render("form");
 });
+
+app.post('/new', (req,res)=>{
+    messages.push({
+        text: req.body.message,
+        user: req.body.userName,
+        added: new Date()
+    })
+
+    res.redirect('/')
+
+})
 
 app.listen(PORT, () => {
   console.log(`The server is running on port: ${PORT}`);
